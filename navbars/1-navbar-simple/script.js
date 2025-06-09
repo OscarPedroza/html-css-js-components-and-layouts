4b8b6188434f96a30b5ac37d069cb8498f7a03a7
Oscar Pedroza <oscarph1802@gmail.com>
2025-06-09 12:50:07 -0600

    Implementar la funcionalidad del modo oscuro

--- navbars/1-navbar-simple/script.js
+++ navbars/1-navbar-simple/script.js
@@ -1,7 +1,21 @@
+// Menu button
 const btnToggle = document.querySelector('.navbar__btn-toggle');
 const navbarMenu = document.querySelector('.navbar__menu');
 
 btnToggle.addEventListener('click', () => {
 	navbarMenu.classList.toggle('active');
 	btnToggle.classList.toggle('active');
+});
+
+// Dark Light mode
+const root = document.documentElement;
+const btnModeToggle = document.getElementById('btn-mode-toggle');
+
+root.classList.toggle('dark-mode', localStorage.getItem('mode') === 'dark');
+
+console.log(localStorage.getItem('mode'));
+
+btnModeToggle.addEventListener('click', () => {
+	const isDark = root.classList.toggle('dark-mode');
+	localStorage.setItem('mode', isDark ? 'dark' : 'light');
 });
