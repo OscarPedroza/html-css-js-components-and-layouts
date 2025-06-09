d4cccb9c198fa47fb30890f606ccff511e842eba
Oscar Pedroza <oscarph1802@gmail.com>
2025-06-08 20:44:16 -0600

    Modificando el index.html de navbars

--- navbars/1-navbar-simple/js/script.js
+++
@@ -1,7 +0,0 @@
-const btnToggle = document.querySelector('.navbar__btn-toggle');
-const navbarMenu = document.querySelector('.navbar__menu');
-
-btnToggle.addEventListener('click', () => {
-	navbarMenu.classList.toggle('active');
-	btnToggle.classList.toggle('active');
-});
