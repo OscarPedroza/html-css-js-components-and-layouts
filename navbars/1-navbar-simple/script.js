fbc7b65bafcff659e44ff5abaf5aaefb1d2d4262
Oscar Pedroza <oscarph1802@gmail.com>
2025-06-09 22:18:32 -0600

    Ajustando la maquetacioń

--- navbars/1-navbar-simple/script.js
+++ navbars/1-navbar-simple/script.js
@@ -1,19 +1,23 @@
-// Menu button
-const btnToggle = document.querySelector('.navbar__btn-toggle');
+/**
+ * Responsive menu
+ */
+
+const btnMenu = document.getElementById('btn-menu');
 const navbarMenu = document.querySelector('.navbar__menu');
 
-btnToggle.addEventListener('click', () => {
+btnMenu.addEventListener('click', () => {
   navbarMenu.classList.toggle('active');
-  btnToggle.classList.toggle('active');
+  btnMenu.classList.toggle('active');
 });
 
-// Dark Light mode
+/**
+ * Dark Light mode
+ */
+
 const root = document.documentElement;
 const btnModeToggle = document.getElementById('btn-mode-toggle');
 
 root.classList.toggle('dark-mode', localStorage.getItem('mode') === 'dark');
-
-console.log(localStorage.getItem('mode'));
 
 btnModeToggle.addEventListener('click', () => {
   const isDark = root.classList.toggle('dark-mode');
