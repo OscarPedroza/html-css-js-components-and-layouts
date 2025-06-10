4edb94d9faddd5a8b53d01ad6fda15eb364614f4
Oscar Pedroza <oscarph1802@gmail.com>
2025-06-09 19:14:09 -0600

    Ajustando la configuración de formato e identación del código

--- navbars/1-navbar-simple/script.js
+++ navbars/1-navbar-simple/script.js
@@ -3,8 +3,8 @@ const btnToggle = document.querySelector('.navbar__btn-toggle');
 const navbarMenu = document.querySelector('.navbar__menu');
 
 btnToggle.addEventListener('click', () => {
-	navbarMenu.classList.toggle('active');
-	btnToggle.classList.toggle('active');
+  navbarMenu.classList.toggle('active');
+  btnToggle.classList.toggle('active');
 });
 
 // Dark Light mode
@@ -16,6 +16,6 @@ root.classList.toggle('dark-mode', localStorage.getItem('mode') === 'dark');
 console.log(localStorage.getItem('mode'));
 
 btnModeToggle.addEventListener('click', () => {
-	const isDark = root.classList.toggle('dark-mode');
-	localStorage.setItem('mode', isDark ? 'dark' : 'light');
+  const isDark = root.classList.toggle('dark-mode');
+  localStorage.setItem('mode', isDark ? 'dark' : 'light');
 });
