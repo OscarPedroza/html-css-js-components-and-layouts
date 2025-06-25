a0ac894e1e41f1aa08737908e66b49b268cfd3b4
Oscar Pedroza <oscarph1802@gmail.com>
2025-06-24 20:09:52 -0600

    Finalizar el navbar con botones

--- navbars/navbar-login-btn/script.js
+++ navbars/navbar-login-btn/script.js
@@ -8,4 +8,20 @@ const menuList = document.getElementById('menu-list');
 btnMenu.addEventListener('click', () => {
   menuList.classList.toggle('active');
   btnMenu.classList.toggle('active');
+
+  navbarButtons.classList.remove('active');
+});
+
+/**
+ * Login toggle
+ */
+
+const btnLogin = document.getElementById('btn-login');
+const navbarButtons = document.getElementById('navbar-buttons');
+
+btnLogin.addEventListener('click', () => {
+  navbarButtons.classList.toggle('active');
+
+  menuList.classList.remove('active');
+  btnMenu.classList.remove('active');
 });
