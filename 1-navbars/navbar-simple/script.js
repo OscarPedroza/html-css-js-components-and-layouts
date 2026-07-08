ca9bf3cad0198e9d86f534394a27baea47235569
Oscar Pedroza <oscarph1802@gmail.com>
2026-07-07 19:44:26 -0600

    dark theme code

--- 1-navbars/navbar-simple/script.js
+++ 1-navbars/navbar-simple/script.js
@@ -10,6 +10,25 @@ btnMenu.addEventListener('click', () => {
   btnMenu.classList.toggle('active');
 });
 
+/**
+ * Theme toggle
+ */
+
+const btnTheme = document.getElementById('btn-theme');
+const savedTheme = localStorage.getItem('theme');
+const html = document.documentElement;
+
+if (savedTheme === 'dark') {
+  html.classList.add('dark');
+}
+
+btnTheme.addEventListener('click', () => {
+  html.classList.toggle('dark');
+
+  // Save theme in localStorage
+  const isDark = html.classList.contains('dark');
+  localStorage.setItem('theme', isDark ? 'dark' : 'light');
+});
+
 // Initialize Lucide Icons
 lucide.createIcons();
-
